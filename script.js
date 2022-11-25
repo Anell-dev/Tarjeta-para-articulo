12bfc97f50fa87e8e905323282125925c8d358bd
Edwin <aneh6272@gmail.com>
2022-11-25 02:01:20 -0500

    logica terminada

--- script.js
+++ script.js
@@ -17,13 +17,11 @@ const presentationMobile = document.querySelector(".options-container-mobile");
 const messageMobile = document.querySelector(".mobile-share-popup");
 
 btnShareMobileBefore.addEventListener("click", (e) => {
-  console.log("hola");
   presentationMobile.classList.toggle("hide");
   messageMobile.classList.toggle("popup-hide");
 });
 
 btnShareMobileAfter.addEventListener("click", (e) => {
-  console.log("hola");
   presentationMobile.classList.toggle("hide");
   messageMobile.classList.toggle("popup-hide");
 });
